5a4983addda9a8c4a157019be859e2ef1b5639b4
powerboat9 <powerboat9@users.noreply.github.com>
2016-04-27 22:12:35 -0400

    Update both.c

--- both.c
+++ both.c
@@ -21,4 +21,20 @@ int connect(char[] address, char[] port) {
     flags.ai_socktype = SOCKET_STREAM;
     getaddrinfo(address, port, &flags, &info);
     int socket = socket(info->ai_family, info->ai_socktype, info->ai_protocol);
-    bind(socket, 
+    connect(socket, info->ai_addr, info->ai_addrlen);
+}
+
+int provide(char[] port) {
+    struct addrinfo *info, flags;
+    memset(&flags, 0, sizeof(flags));
+    flags.ai_family = AF_UNSPEC;
+    flags.ai_socktype = SOCKET_STREAM;
+    flags.ai_flags = AI_PASSIVE;
+    getaddrinfo(NULL, port, &flags, &info);
+    int socket = socket(info->ai_family, info->ai_socktype, info->ai_protocol);
+    bind(socket, info->ai_addr, info->ai_addrlen);
+    listen(socket, 5);
+    accept(
+    return newSocket;
+}
+    
